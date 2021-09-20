64627d282580e0886e7d69222518a36ccd78dc8f
capgteam <capgteam22020@gamil.com>
2021-09-20 13:03:00 +0530

    test class added

---
+++ sampleapp/src/main/java/org/cap/demo/TestClass.java
@@ -0,0 +1,12 @@
+package org.cap.demo;
+
+public class TestClass {
+	
+	public void test() {
+		System.out.println("Test class validated");
+		System.out.println("All test functionalies goes here...........");
+		System.out.println("Test class validated");
+		System.out.println("Test class validated");
+	}
+
+}
